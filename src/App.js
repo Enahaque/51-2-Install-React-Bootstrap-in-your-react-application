5e6acf31b94b4890b5d53a66fc10d38490cf74b9
Enahaque <enahaque1164@gmil.com>
2022-06-21 01:13:53 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Button } from 'react-bootstrap';
 import { Spinner } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
+import { Toast } from 'react-bootstrap';
 import './App.css';
 import CardGroup2 from './components/CardGroup2/CardGroup2';
 
@@ -38,10 +41,46 @@ function App() {
         <Spinner animation="grow" variant="light" />
         <Spinner animation="grow" variant="dark" />
       </>
-      
+
       <CardGroup2></CardGroup2>
+
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Hurrah!! I'd it!!</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
+      <br />
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <br />
+      <Toast>
+        <Toast.Header>
+          <img src='https://i.ibb.co/SyC2nWt/500.jpg' className="rounded me-2" alt="" />
+          <strong className="me-auto">Bootstrap</strong>
+          <small>11 mins ago</small>
+        </Toast.Header>
+        <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+      </Toast>
     </div>
-    
+
   );
 }
 
